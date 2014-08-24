d8378216b017c44c0628de30334ca6906e15cbd2
Yicheng Fei <fycisc@gmail.com>
2014-08-24 18:31:09 +0800

    static configration added

--- IGEMServer/settings.py
+++ IGEMServer/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
