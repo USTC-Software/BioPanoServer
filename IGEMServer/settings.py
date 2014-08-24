361d63742c80cd3bcf3eda3b1a2df5675bd679fb
Yicheng Fei <fycisc@gmail.com>
2014-08-24 18:56:14 +0800

    static file added to urls.py(trial 5)

--- IGEMServer/settings.py
+++ IGEMServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '7jkomavmtmlifw$pbs9@wu%y^=sn37xv0on+z9@0oqp0_g7ilu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
