7505a823cdb071e43aa23a2cfb67c4933e14a2dc
Yicheng Fei <fycisc@gmail.com>
2014-08-24 18:44:36 +0800

    static file added to urls.py(trial 2)

--- IGEMServer/urls.py
+++ IGEMServer/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import settings
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}, name='static'),
-)
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}, name='static'),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
