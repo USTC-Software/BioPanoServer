721ee38da789e4a50d62df04e1deac4d906dff43
Yicheng Fei <fycisc@gmail.com>
2014-08-29 13:03:23 +0800

    COMPLETE view back to default

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # some other urls
-    url(r'^complete/(?P<backend>[^/]+)/$', AuthComplete.as_view()),
-    url(r'^login-error/$', LoginError.as_view()),
+    # url(r'^complete/(?P<backend>[^/]+)/$', AuthComplete.as_view()),
+    # url(r'^login-error/$', LoginError.as_view()),
     url(r'', include('social_auth.urls')),
 )
