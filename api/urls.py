d67ce8d8f8531882dac3f15302dd29a8a86a6be3
Yifan Gao <yifan@YifandeMacBook-Air.local>
2014-09-02 14:09:40 +0800

    fix url order for link_ref and search

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,10 @@ import views
 
 urlpatterns = patterns('',
     url(r'^node/$', views.add_node),    # POST
-    url(r"^node/(?P<id>[^/]+)/$", views.del_or_addref_node),  # DELETE / PUT
     url(r'^node/search/$', views.search_json_node),    # POST
+    url(r"^node/(?P<id>[^/]+)/$", views.del_or_addref_node),  # DELETE / PUT
     url(r'^link/$', views.add_link),    # POST
-    url(r'^link/(?P<id>[^/]+)/$', views.del_or_addref_link),  # DELETE / PUT
     url(r'^link/search/$', views.search_json_link),  # POST
+    url(r'^link/(?P<id>[^/]+)/$', views.del_or_addref_link),  # DELETE / PUT
+
 )
