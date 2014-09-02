16cd15cab8ea41ba000dedadb1dbdc4227b2922e
Yicheng Fei <fycisc@gmail.com>
2014-09-02 23:51:25 +0800

    fix a small bug in api (delete some slashes)

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^/(?P<species>[\w]+)/node/$', views.add_node),    # POST
-    url(r"^/(?P<species>[\w]+)/node/(?P<id>[\w]+)/$", views.del_or_addref_node),  # DELETE / PUT
+    url(r'^(?P<species>[\w]+)/node/$', views.add_node),    # POST
+    url(r"^(?P<species>[\w]+)/node/(?P<id>[\w]+)/$", views.del_or_addref_node),  # DELETE / PUT
 
-    url(r'^/(?P<species>[\w]+)/link/$', views.add_link),    # POST
-    url(r'^/(?P<species>[\w]+)/link/(?P<id>[\w]+)/$', views.del_or_addref_link),  # DELETE / PUT
+    url(r'^(?P<species>[\w]+)/link/$', views.add_link),    # POST
+    url(r'^(?P<species>[\w]+)/link/(?P<id>[\w]+)/$', views.del_or_addref_link),  # DELETE / PUT
 
 )
