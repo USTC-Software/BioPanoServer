9ae936e775fc35f5374a68f3bca55a464e3f0ba4
Yicheng Fei <fycisc@gmail.com>
2014-09-06 17:29:30 +0800

    delete species

--- api/url_data.py
+++ api/url_data.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<species>[\w]+)/node/$', views.add_node),    # POST
-    url(r"^(?P<species>[\w]+)/node/(?P<id>[\w]+)/$", views.get_del_addref_node),  # DELETE / PUT / GET
+    url(r'^node/$', views.add_node),    # POST
+    url(r"^node/(?P<id>[\w]+)/$", views.get_del_addref_node),  # DELETE / PUT / GET
 
-    url(r'^(?P<species>[\w]+)/link/$', views.add_link),    # POST
-    url(r'^(?P<species>[\w]+)/link/(?P<id>[\w]+)/$', views.get_del_addref_link),  # DELETE / PUT / GET
+    url(r'^link/$', views.add_link),    # POST
+    url(r'^link/(?P<id>[\w]+)/$', views.get_del_addref_link),  # DELETE / PUT / GET
 
 )
