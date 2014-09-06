9901a44d20dac0c6bb6a0d18f23599e11e9e0e0d
Yicheng Fei <fycisc@gmail.com>
2014-09-06 17:36:23 +0800

    fix a bug in search url 2

--- api/url_search.py
+++ api/url_search.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^/node/$', views.search_json_node),    # POST
-    url(r'^/link/$', views.search_json_link),  # POST
+    url(r'^node/$', views.search_json_node),    # POST
+    url(r'^link/$', views.search_json_link),  # POST
 )
 
