01cebbd9fa7aca5d977eb58aae8307088169f55a
Yicheng Fei <fycisc@gmail.com>
2014-09-06 21:22:59 +0800

    urlpatterns= (+patterns)

--- my_auth/url_auth.py
+++ my_auth/url_auth.py
@@ -3,7 +3,7 @@ __author__ = 'feiyicheng'
 from django.conf.urls import patterns, include, url
 import views
 
-urlpatterns = ('',
+urlpatterns = patterns('',
     # url(r'^oauth/authorize/$', ),
     url(r'^oauth/google/login/$', 'my_auth.view_oauth.login_start_google'),
     url(r'^oauth/google/complete/$', 'my_auth.view_oauth.login_complete_google'),
