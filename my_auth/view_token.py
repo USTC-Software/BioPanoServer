fa9a2baa1087c3b0e4284dbaaf6ca864ee40566a
Yicheng Fei <fycisc@gmail.com>
2014-09-07 21:08:13 +0800

    测试token机制

--- my_auth/view_token.py
+++ my_auth/view_token.py
@@ -1,5 +1,21 @@
 __author__ = 'feiyicheng'
 
 from django.shortcuts import HttpResponse, HttpResponsePermanentRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+import json
 
+
+def login(request):
+    para = request.REQUEST
+    if ('username', 'password') in para.keys():
+        user = User.objects.get(username=para['username'], password=para['password'])
+        token = default_token_generator.make_token(user)
+        data = {
+            'status': 'success',
+            'token': token,
+            'user': user.pk
+        }
+        return HttpResponse(json.dumps(data))
+    return HttpResponse('hehe')
 
