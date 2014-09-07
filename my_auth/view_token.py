3fb5b6f9e44150683c53e8192c6363a4a8962386
Yicheng Fei <fycisc@gmail.com>
2014-09-07 21:19:46 +0800

    测试token机制 5

--- my_auth/view_token.py
+++ my_auth/view_token.py
@@ -2,6 +2,7 @@ __author__ = 'feiyicheng'
 
 from django.shortcuts import HttpResponse, HttpResponsePermanentRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.contrib.auth.tokens import default_token_generator
 import json
 
@@ -9,15 +10,22 @@ import json
 def login(request):
     para = request.REQUEST
     if 'username' in para.keys() and 'password' in para.keys():
-        user = User.objects.get(username=para['username'], password=para['password'])
-        return HttpResponse(user.username + 'heh')
+        user = User.objects.get(username=para['username'])
+        if user.check_password(para['password']):
+            # return HttpResponse(user.username + 'heh')
 
-        token = default_token_generator.make_token(user)
-        data = {
-            'status': 'success',
-            'token': token,
-            'user': user.pk
-        }
+            token = default_token_generator.make_token(user)
+            data = {
+                'status': 'success',
+                'token': token,
+                'user': user.pk
+            }
+        else:
+            data = {
+                'status': 'error',
+                'reason': 'password not correct'
+            }
         return HttpResponse(json.dumps(data))
+    
     return HttpResponse('hehe' + str(para))
 
