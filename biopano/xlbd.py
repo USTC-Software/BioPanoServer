dbcb9a97f259ef620f2590bccbc370b336c583e0
beibeihome <beibeihome88@gmail.com>
2014-09-15 19:27:10 +0800

    stop recoding index information and change the way to check database according sequence directly

--- biopano/xlbd.py
+++ biopano/xlbd.py
@@ -16,46 +16,15 @@ def NeedleWunch(s, t):
     n = len(s)
     m = len(t)
     f = [[0 for j in range(m + 1)] for i in range(n + 1)]
-    g = [[0 for j in range(m + 1)] for i in range(n + 1)]
-    h = [[0 for j in range(m + 1)] for i in range(n + 1)]
 
     for i in range(n):
         f[i + 1][0] = -2 * (i + 1)
-        g[i + 1][0] = i
-        h[i + 1][0] = 0
     for j in range(m):
         f[0][j + 1] = -2 * (j + 1)
-        g[0][j + 1] = 0
-        h[0][j + 1] = j
 
     for i in range(n):
         for j in range(m):
             f[i + 1][j + 1] = max(max(f[i + 1][j], f[i][j + 1]) - 2, f[i][j] + func(s[i], t[j]))
-            if f[i + 1][j + 1] == f[i + 1][j] - 2:
-                g[i + 1][j + 1] = i + 1
-                h[i + 1][j + 1] = j
-            elif f[i + 1][j + 1] == f[i][j + 1] - 2:
-                g[i + 1][j + 1] = i
-                h[i + 1][j + 1] = j + 1
-            else:
-                g[i + 1][j + 1] = i
-                h[i + 1][j + 1] = j
-    i = n
-    j = m
-    ss = ""
-    tt = ""
-    while i != 0 or j != 0:
-        if g[i][j] == i:
-            ss = '_' + ss
-        else:
-            ss = s[i - 1] + ss
-        if h[i][j] == j:
-            tt = '_' + tt
-        else:
-            tt = t[j - 1] + tt
-        i, j = g[i][j], h[i][j]
-    # print ss
-    #print tt
     return f[n][m]
 
 
@@ -72,25 +41,29 @@ def main(request):
         for u_t_r in db.u_t_r.find():
             if u_t_r['TYPE'] == 'O_T_P':
                 if u_t_r['SEQUENCE_5'] is not '':
-                    id_list.append(str(u_t_r['node_id']))
+                    # id_list.append(str(u_t_r['node_id']))
                     b.append(u_t_r['SEQUENCE_5'].upper())
                 if u_t_r['SEQUENCE_3'] is not '':
-                    id_list.append(str(u_t_r['node_id']))
+                    # id_list.append(str(u_t_r['node_id']))
                     b.append(u_t_r['SEQUENCE_3'].upper())
             else:
-                id_list.append(str(u_t_r['node_id']))
+                # id_list.append(str(u_t_r['node_id']))
                 b.append(u_t_r['SEQUENCE'].upper())
         ans = -9999999999
         ansx = []
         for i in b:
             x = NeedleWunch(a, i)
             if x == ans:
-                ansx += [b.index(i)]
+                ansx += [b]
             if x > ans:
                 ans = x
-                ansx = [b.index(i)]
+                ansx = [b]
 
         result = []
+        for sequence in ansx:
+            for node in db.u_t_r.find({'$or': [{'SEQUENCE': sequence}, {'SEQUENCE_3': sequence}, {'SEQUENCE_5': sequence}]}):
+                if node['_id'] not in id_list:
+                    id_list.append(node['_id'])
         for each in ansx:
             dicts = {'_id': id_list[each]}
             node = db.node.find_one({'_id': bson.ObjectId(id_list[each])})
