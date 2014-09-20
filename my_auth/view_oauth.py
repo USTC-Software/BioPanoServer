d0e6d5ee52bd46740458c74b9d132133693a7d6e
Yifan Gao <git@gaoyifan.com>
2014-09-20 18:32:43 +0800

    fix a bug: token

--- my_auth/view_oauth.py
+++ my_auth/view_oauth.py
@@ -52,11 +52,11 @@ def login_complete_google(request):
     # login the user
     # return HttpResponse('profile get\n' + str(profile))
 
-    user, token = _get_user_and_token(profile)
+    (user, token) = _get_user_and_token(profile)
     if user:
         data = {
             'status': 'success',
-            'token': str(token),
+            'token': token,
             'user': user.pk,
         }
     else:
@@ -93,7 +93,7 @@ def _get_user_and_token(profile):
 
     user, created = User.objects.get_or_create(username=profile['email'])
     _update_user(user, profile)
-    token = Token.objects.get_or_create(user=user)
+    token, created = Token.objects.get_or_create(user=user)
     return (user, token) if user else (None, None)
 
 
