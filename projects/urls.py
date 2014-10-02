5e7b8bda991f1c2231704c73ce1050e78bdfd9ee
费奕程 <feiyicheng@Fei-Yicheng.local>
2014-10-02 22:03:34 +0800

    unify project apis

--- projects/urls.py
+++ projects/urls.py
@@ -12,9 +12,9 @@ urlpatterns = patterns('',
     # url(r'^create/(?P<prj_name>[^/]+)/$', 'projects.views.create_project'),  # create a new project
     # url(r'^addin/(?P<prj_id>[^/]+)$', 'projects.views.add_in_a_project'), # add the user into a project
     url(r'^delete/(?P<prj_id>\d+)/$', 'projects.views.del_project'), # delete a project
-    url(r'^project/(?P<prj_id>[^/]+)/collaborator/(?P<uid>[^/]+)/$', 'projects.views.add_or_del_collaborator'),
     url(r'^modify/(?P<prj_id>[^/]+)/$', 'projects.views.modify_project'),
     url(r'^project/$', 'projects.views.list_or_create'),
     url(r'^project/(?P<prj_id>[^/]+)/$','projects.views.get_one'),
+    url(r'^(?P<prj_id>[^/]+)/collaborator/(?P<uid>[^/]+)/$', 'projects.views.add_or_del_collaborator'),
     # url(r'^switch/(?P<prj_id>[^/]+)/$', 'projects.views.switch_project'),
 )
