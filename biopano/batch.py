a358ccb82dd354c1ee8a9d80fe130914bf513d76
beibeihome <beibeihome88@gmail.com>
2014-10-10 20:42:58 +0800

    just finish batch api and keep untested

--- biopano/batch.py
+++ biopano/batch.py
@@ -4,24 +4,25 @@ from api.views import get_del_addref_node, add_node, add_link, get_del_addref_li
 from django.shortcuts import HttpResponse
 from django.http import QueryDict
 import json
+import urllib
 
 
 def node_batch(request):
     if request.method == 'PATCH':
         #return HttpResponse('Please relocate node by PATCH request')
         try:
-            body_list = QueryDict(request.body)
+            body = QueryDict(request.body)
         except AttributeError:
             return HttpResponse('json.loads failed')
-
-        for body in body_list:
+        para_list = json.loads(body['para_list'])
+        for para in para_list:
             sub_request = request
-            sub_request.body = body
-            id = body['id']
+            sub_request.body = urllib.urlencode(para)
+            _id = para['id']
 
-            receiver = get_del_addref_node(sub_request, id)
+            receiver = get_del_addref_node(sub_request, _id)
             if receiver.content != "{'status': 'success}":
-                return HttpResponse("{'status': 'error', 'id': " + id + "}")
+                return HttpResponse("{'status': 'error', 'id': " + _id + "}")
         return HttpResponse("{'status': 'success}")
 
     elif request.method == 'POST':
@@ -38,35 +39,64 @@ def node_batch(request):
         return HttpResponse(result_text)
 
     elif request.method == 'DELETE':
-        ref_id_list = json.loads(request.body)
+        body = QueryDict(request.body)
+        ref_id_list = json.loads(body['para_list'])
         for ref_id in ref_id_list:
             sub_request = request
             sub_request.body = ''
-            get_del_addref_node(sub_request, ref_id)
+            get_del_addref_node(sub_request, ref_id['id'])
 
     elif request.method == 'PUT':
-        para_list = QueryDict(request.body)
+        body = QueryDict(request.body)
+        para_list = json.loads(body['para_list'])
+        result_list = []
+        for para in para_list:
+            sub_request = request
+            sub_request.body = urllib.urlencode(para)
+            receiver = get_del_addref_node(sub_request)
+            result_list.append(json.loads(receiver.content))
+        result_text = json.dumps(result_list)
+        return HttpResponse(result_text)
+
 
 def link_batch(request):
-	if request.method == 'POST':
-		#return HttpResponse('{"function": "add_batch", "error": "POST method is requested"}')
-		para_list = request.POST['para_list']
-		para_list = json.loads(para_list)
-		result_list = []
-		for para in para_list:
-			sub_request = request
-			sub_request.POST = para
+    if request.method == 'POST':
+        #return HttpResponse('{"function": "add_batch", "error": "POST method is requested"}')
+        para_list = request.POST['para_list']
+        para_list = json.loads(para_list)
+        result_list = []
+        for para in para_list:
+            sub_request = request
+            sub_request.POST = para
 
-			receiver = add_link(sub_request)
-			result_list.append(json.laads(receiver.content))
-		result_text = json.dumps(result_list)
-		return HttpResponse(result_text)
+            receiver = add_link(sub_request)
+            result_list.append(json.loads(receiver.content))
+        result_text = json.dumps(result_list)
+        return HttpResponse(result_text)
 
+    elif request.method == 'DELETE':
+        body = QueryDict(request.body)
+        para_list = json.loads(body['para_list'])
+        result_list = []
+        for para in para_list:
+            sub_request = request
+            sub_request.body = urllib.urlencode(para)
+            receiver = get_del_addref_link(sub_request)
+            result_list.append(json.loads(receiver.content))
+        result_text = json.dumps(result_list)
+        return HttpResponse(result_text)
 
-def delete(request):
-	if request.method != 'DELETE':
-		return HttpResponse('{"function": "add_batch", "error": "POST method is requested"}')
-
+    elif request.method == 'PUT':
+        body = QueryDict(request.body)
+        para_list = json.loads(body['para_list'])
+        result_list = []
+        for para in para_list:
+            sub_request = request
+            sub_request.body = urllib.urlencode(para)
+            receiver = get_del_addref_link(sub_request, para_list['id'])
+            result_list.append(json.loads(receiver.content))
+        result_text = json.dumps(result_list)
+        return HttpResponse(result_text)
 
 
 
--- biopano/views.py
+++ biopano/views.py
@@ -65,7 +65,8 @@ def look_around(request, **kwargs):
 
 def request_show(request):
     body = request.body
-    paras = QueryDict(body)
+    para_list = QueryDict(body)
+
     #paras.get('id1')
     result = 'body:\t ' + body + '\nparas:\n' + str(paras) + '\nid1:\t' + str(paras.get('id1')) + '\nrequest:\n' + str(request)
 
