9ab715f39cb0a50c2c449ce9fe32bf2a895be7cb
Yifan Gao <git@gaoyifan.com>
2014-10-11 05:32:12 +0800

    modify mysql host setting

--- IGEMServer/settings.py
+++ IGEMServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'master',
         'PASSWORD': 'SyntheticBiology',
-        'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'mysql',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',  # Set to empty string for default.
     }
 }
