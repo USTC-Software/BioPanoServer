3192b7dac2cb699e941e1d9ab8a6ba8b32da171d
费奕程 <feiyicheng@Fei-Yicheng.local>
2014-10-19 14:35:38 +0800

    fix a bug(delete models in mysql)

--- projects/models.py
+++ projects/models.py
@@ -7,22 +7,22 @@ import mongoengine.fields as fields
 from mongoengine.django.auth import User
 
 
-class Project(models.Model):
-    """
-    a class that represents a project in which user can cooperate together
-
-    """
-    author = models.ForeignKey(User, related_name='projects_authored')
-    collaborators = models.ManyToManyField(User, blank=True, related_name='projects_collaborated')
-    name = models.CharField(max_length=40)
-    description = models.TextField(max_length=300, blank=True, default="no description yet")
-    species = models.TextField(max_length=100, blank=True)
-    is_active = models.BooleanField()  # this field will be set False if it is 'deleted' ,to keep prj_id
-                                        # still all the time
-
-    def __unicode__(self):
-        return "name:" + self.name + " author:" + self.author['username']
-
+# class Project(models.Model):
+#     """
+#     a class that represents a project in which user can cooperate together
+#
+#     """
+#     author = models.ForeignKey(User, related_name='projects_authored')
+#     collaborators = models.ManyToManyField(User, blank=True, related_name='projects_collaborated')
+#     name = models.CharField(max_length=40)
+#     description = models.TextField(max_length=300, blank=True, default="no description yet")
+#     species = models.TextField(max_length=100, blank=True)
+#     is_active = models.BooleanField()  # this field will be set False if it is 'deleted' ,to keep prj_id
+#                                         # still all the time
+#
+#     def __unicode__(self):
+#         return "name:" + self.name + " author:" + self.author['username']
+#
 
 class ProjectFile(Document):
     """
@@ -36,14 +36,20 @@ class ProjectFile(Document):
     species = fields.StringField(max_length=100, default="")
     is_active = fields.BooleanField(default=True)
 
-
-class UserProfile(models.Model):
+class UserFile(Document):
     """
     Profile of user that stores extra info of User
     """
-    user = models.OneToOneField(User, related_name='userprofile')
-    currentProject = models.ForeignKey(Project, blank=True, related_name='+')  # just store the info of the latest prj
+    user = fields.ReferenceField(User, required=True)
 
+#
+# class UserProfile(models.Model):
+#     """
+#     Profile of user that stores extra info of User
+#     """
+#     user = models.OneToOneField(User, related_name='userprofile')
+#     currentProject = models.ForeignKey(Project, blank=True, related_name='+')  # just store the info of the latest prj
+#
 
 
 
